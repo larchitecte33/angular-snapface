cba59a36f66faffb02da99b48d20130c3ff9f13f
Gauthier Dubourdieu <gdubourd@gmail.com>
2022-02-24 17:22:32 +0100

    Ajout des composants header et face-snap-list ainsi que du service face-snaps.service

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FaceSnap } from '../models/face-snap.model';
+import { FaceSnapsService } from '../services/face-snaps.service';
 
 @Component({
   selector: 'app-face-snap',
@@ -11,24 +12,26 @@ export class FaceSnapComponent {
   
   isSnaped!: boolean;
   texteSnap!: string;
+  jungle!: string;
+
+  constructor(private faceSnapsService: FaceSnapsService) {}
 
   ngOnInit() {
     this.isSnaped = false;
     this.texteSnap = 'Oh Snap!';  
+    this.jungle = 'welcome to the jungle';
   }
 
   onClickSnap() {
     if(this.isSnaped) {
-      //this.faceSnap.snaps--;
+      this.faceSnapsService.snapFaceSnapById(this.faceSnap.id, 'unsnap');
       this.texteSnap = 'Oh Snap!';
     }
     else {
-      //this.faceSnap.snaps++;
+      this.faceSnapsService.snapFaceSnapById(this.faceSnap.id, 'snap');
       this.texteSnap = 'Oops, unsnap...';
     }
 
     this.isSnaped = !this.isSnaped;
-
-    this.faceSnap.snaps++;
   }
 }
